ba4fb971755e1eb85a063ea144c75596ddd6186a
Alexander <siegfried1997@gmail.com>
2022-07-06 01:04:18 +0400

    added swagger

---
+++ WebApiTestTask/Common/Swagger/SwaggerConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApiTestTask.Common.Swagger
+{
+    public static class SwaggerConfiguration
+    {
+        public static void ConfigureSwagger(this IServiceCollection services) 
+        {
+            services.AddSwaggerDocument(c => 
+            {
+                c.Title = "Dresses";
+                c.DocumentName 
+            })
+        }
+    }
+}
--- WebApiTestTask/Program.cs
+++ WebApiTestTask/Program.cs
@@ -22,4 +22,7 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
+app.UseOpenApi();
+app.UseSwaggerUi3();
+
 app.Run();
